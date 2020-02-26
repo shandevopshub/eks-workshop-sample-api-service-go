11e06ddb220d5a82e1a6198796c74e2765bbd82c
shandevopshub <57555304+shandevopshub@users.noreply.github.com>
2020-02-26 16:14:02 +0530

    COMMIT
    COMMIT

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World new file "}
+		res := &response{Message: "Hello World new file for test "}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
